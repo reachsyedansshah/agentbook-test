17b85b7738cbab77870d246b683ea1112ff922e3
syedansshah <syedanasshah@yahoo.com>
2024-03-24 14:02:33 +0400

    updated user request validator

--- Backend/validators/userValidator.js
+++ Backend/validators/userValidator.js
@@ -3,7 +3,8 @@ const Joi = require('joi');
 const updateUserValidationSchema = Joi.object({
     name: Joi.string().optional(),
     email: Joi.string().email().optional(),
-    password: Joi.string().min(6).optional()
+    password: Joi.string().min(6).optional(),
+    avatar: Joi.string().optional(),
 });
 
 module.exports = {  updateUserValidationSchema };
