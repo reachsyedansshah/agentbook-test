247220b478e05d10f59887d618885a077d43298c
syedansshah <syedanasshah@yahoo.com>
2024-03-24 14:01:45 +0400

    update config app host

--- Backend/config/index.js
+++ Backend/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-  APP_HOST: process.env.APP_HOST || "http://localhost:",
+  APP_HOST: process.env.APP_HOST || "http://localhost:4001",
   env: process.env.NODE_ENV || 'development',
   MONGO_URI: process.env.MONGO_URI,
   PORT: process.env.PORT || 3001
